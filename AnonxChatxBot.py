b4734c76648f58c9d267a5f3849c2fbaa7dc578e
DanyCapone <filtersdeep@gmail.com>
2025-02-18 04:41:42 -0600

    AnonxChatxBot.py

--- AnonxChatxBot.py
+++ AnonxChatxBot.py
@@ -1,28 +1,24 @@
 from telegram import Update
-from telegram.ext import Updater, MessageHandler, Filters, CallbackContext
+from telegram.ext import Application, MessageHandler, filters
 
 TOKEN = "7617668499:AAHYoP6UEBMCb9t2axjrYTXkY01Q9bHdfCE"
 CHAT_ID = -2259374958  # Reemplaza con el ID de tu grupo o canal
 
-def forward_message(update: Update, context: CallbackContext) -> None:
+async def forward_message(update: Update, context) -> None:
     user = update.message.from_user
     text = update.message.text
 
-    # Formateamos el mensaje con el nombre del usuario
     message_to_send = f"{user.first_name} dice:\n{text}"
     
-    # Enviar el mensaje al grupo/canal
-    context.bot.send_message(chat_id=CHAT_ID, text=message_to_send)
+    await context.bot.send_message(chat_id=CHAT_ID, text=message_to_send)
 
 def main():
-    updater = Updater(TOKEN, use_context=True)
-    dp = updater.dispatcher
+    app = Application.builder().token(TOKEN).build()
 
-    # Manejar todos los mensajes de los usuarios
-    dp.add_handler(MessageHandler(Filters.text & ~Filters.command, forward_message))
+    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, forward_message))
 
-    updater.start_polling()
-    updater.idle()
+    print("Bot iniciado...")
+    app.run_polling()
 
 if __name__ == '__main__':
     main()
